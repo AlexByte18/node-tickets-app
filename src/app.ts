e699f87aeab5eee55a11ccf077eed8dc67bd40a0
AlexByte <alex.g.cortes.g@gmail.com>
2024-01-03 23:11:31 -0600

    add and implemets wss service

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
+import { createServer } from 'http';
 import { envs } from './config/envs';
 import { AppRoutes } from './presentation/routes';
 import { Server } from './presentation/server';
+import { WssService } from './presentation/services/wss.service';
+import { env } from 'process';
 
 
 (async()=> {
@@ -15,5 +18,11 @@ function main() {
     routes: AppRoutes.routes,
   });
 
-  server.start();
+  const httpServer = createServer(server.app);
+
+  WssService.initWss({server: httpServer});
+
+  httpServer.listen(envs.PORT, () => {
+    console.log(`server running on port ${envs.PORT}`);
+  })
 }
